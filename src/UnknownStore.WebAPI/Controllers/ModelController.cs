31e56f3001c9862418909267a94c08a2d94cc0ba
Pavel <pavell.urusov@gmail.com>
2022-05-08 22:59:39 +0300

    Created model controller and action add-model

---
+++ src/UnknownStore.WebAPI/Controllers/ModelController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UnknownStore.BusinessLogic.CQRS.Commands.ModelCommands.CreateModel;
+using UnknownStore.Common.Constants;
+using UnknownStore.Common.DataTransferObjects;
+
+namespace UnknownStore.WebAPI.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("store/[controller]")]
+    public class ModelController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ModelController(
+            IMediator mediator
+        )
+        {
+            _mediator = mediator;
+        }
+
+        [Authorize(Policy = nameof(Roles.Manager))]
+        [HttpGet]
+        [Route("add-model")]
+        public async Task<IActionResult> CreateModel([FromForm] CreateModelDto request)
+        {
+            var response = await _mediator.Send(new CreateModelCommand(request));
+            return StatusCode((int)response.StatusCode, response);
+        }
+    }
+}
