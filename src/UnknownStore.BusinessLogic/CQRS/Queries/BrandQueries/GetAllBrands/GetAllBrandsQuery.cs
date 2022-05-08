2c5d62bc7659790ce339bb9d17f4ba3cc784f7de
Pavel <pavell.urusov@gmail.com>
2022-05-08 04:12:55 +0300

    Created query,controller,dto for brand
    made an GetAllBrandQuery, BrandController, GetBrandDto and action get-all

---
+++ src/UnknownStore.BusinessLogic/CQRS/Queries/BrandQueries/GetAllBrands/GetAllBrandsQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using UnknownStore.Common.CQRS;
+
+namespace UnknownStore.BusinessLogic.CQRS.Queries.BrandQueries.GetAllBrands
+{
+    public record GetAllBrandsQuery : IRequest<ResponseBase>;
+}
