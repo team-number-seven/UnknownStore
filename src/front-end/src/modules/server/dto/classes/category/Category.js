833370b6e4b6e9640a915e7efc3372acccf4d47d
Sergey <sergeimurin29@gmail.com>
2022-05-10 20:19:31 +0300

    added 'gender'

--- src/front-end/src/modules/server/dto/classes/category/Category.js
+++ src/front-end/src/modules/server/dto/classes/category/Category.js
@@ -1,8 +1,9 @@
 import {AgeType} from "./AgeType";
+import {Gender} from "./sub-category/Gender";
 import {SubCategories} from "./sub-category/SubCategories";
 
 export class Category {
-    constructor(dtoObject, fields = ['categoryId', 'title', 'subCategories', 'ageType']) {
+    constructor(dtoObject, fields = ['categoryId', 'title', 'subCategories', 'ageType', 'gender']) {
         for (let field of fields) {
             for (let key in dtoObject) {
                 if (field === key && !Array.isArray(dtoObject[key])) {
@@ -14,6 +15,9 @@ export class Category {
                     }
                     if (key === 'ageType') {
                         this[key] = new AgeType(dtoObject[key]);
+                    }
+                    if (key === 'gender') {
+                        this[key] = new Gender(dtoObject[key]);
                     }
                 }
             }
--- src/front-end/src/modules/server/dto/classes/category/sub-category/SubCategory.js
+++ src/front-end/src/modules/server/dto/classes/category/sub-category/SubCategory.js
@@ -3,16 +3,13 @@ import {Size} from "./Size";
 
 
 export class SubCategory {
-    constructor(dtoObject, fields = ['ageCategoryId', 'gender', 'size', 'title']) {
+    constructor(dtoObject, fields = ['ageCategoryId', 'size', 'title']) {
         for (let field of fields) {
             for (let key in dtoObject) {
                 if (field === key && !Array.isArray(dtoObject[key])) {
                     this[key] = dtoObject[key];
                     break;
                 } else {
-                    if (key === 'gender') {
-                        this[key] = new Gender(dtoObject[key]);
-                    }
                     if (key === 'size') {
                         this[key] = new Size(dtoObject[key]);
                     }
