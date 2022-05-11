740c7f5faa0040b33a275b7b77f596814b6ae6af
Pavel <pavell.urusov@gmail.com>
2022-05-11 09:41:59 +0300

    Created query,action and dto.

---
+++ src/UnknownStore.BusinessLogic/CQRS/Queries/ModelQueries/GetModelById/GetModelByIdValidator.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using UnknownStore.Common.CQRS.Validation;
+using UnknownStore.DAL.Interfaces;
+
+namespace UnknownStore.BusinessLogic.CQRS.Queries.ModelQueries.GetModelById
+{
+    public class GetModelByIdValidator : IValidationHandler<GetModelByIdQuery>
+    {
+        private readonly IStoreDbContext _context;
+
+        public GetModelByIdValidator(
+            IStoreDbContext context
+        )
+        {
+            _context = context;
+        }
+
+        public async Task<ValidationResult> Validate(GetModelByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Models.FindAsync(request.ModelId) is null
+                ? ValidationResult.Fail(ValidationMessenger.NotFoundEntity(nameof(request.ModelId)))
+                : ValidationResult.Success;
+        }
+    }
+}
---
+++ src/UnknownStore.Common/DataTransferObjects/Get/GetModelDto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using UnknownStore.Common.Mappings;
+using UnknownStore.DAL.Entities.Store;
+
+namespace UnknownStore.Common.DataTransferObjects.Get
+{
+    public class GetModelDto : IMapWith<Model>
+    {
+        public Guid ModelId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public GetBrandDto Brand { get; set; }
+        public GetSubCategoryDto SubCategory { get; set; }
+        public GetColorDto Color { get; set; }
+        public GetFactoryDto Factory { get; set; }
+        public GetSeasonDto Season { get; set; }
+        public FileContentResult MainImage { get; set; }
+        public IEnumerable<FileContentResult> Images { get; set; }
+        public IDictionary<double, int> AmountOfSize { get; set; }
+        public IDictionary<string, string> ModelData { get; set; }
+
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Model, GetModelDto>()
+                .ForMember(dto => dto.ModelId, opt => opt.MapFrom(m => m.Id))
+                .ForMember(dto => dto.Title, opt => opt.MapFrom(m => m.Title))
+                .ForMember(dto => dto.Description, opt => opt.MapFrom(m => m.Description))
+                .ForMember(dto => dto.Price, opt => opt.MapFrom(m => m.Price))
+                .ForMember(dto => dto.Brand, opt => opt.Ignore())
+                .ForMember(dto => dto.SubCategory, opt => opt.Ignore())
+                .ForMember(dto => dto.Color, opt => opt.Ignore())
+                .ForMember(dto => dto.Factory, opt => opt.Ignore())
+                .ForMember(dto => dto.Season, opt => opt.Ignore())
+                .ForMember(dto => dto.MainImage, opt => opt.Ignore())
+                .ForMember(dto => dto.Images, opt => opt.Ignore())
+                .ForMember(dto => dto.AmountOfSize,
+                    opt => opt.MapFrom(m => m.AmountOfSizes.ToDictionary(am => am.Value, am => am.Amount)))
+                .ForMember(dto => dto.ModelData,
+                    opt => opt.MapFrom(m => m.ModelData.ToDictionary(md => md.Key, md => md.Value)));
+        }
+    }
+}
