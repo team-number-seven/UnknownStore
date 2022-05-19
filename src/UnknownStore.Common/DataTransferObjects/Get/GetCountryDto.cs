112cab4886f3165bca36cc9bda4f4a3f1aef85f2
Pavel <pavell.urusov@gmail.com>
2022-05-19 23:49:27 +0300

    added ctities to counrtryDto

--- src/UnknownStore.Common/DataTransferObjects/Get/GetCountryDto.cs
+++ src/UnknownStore.Common/DataTransferObjects/Get/GetCountryDto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using AutoMapper;
 using UnknownStore.Common.Mappings;
 using UnknownStore.DAL.Entities.Store;
@@ -9,13 +10,15 @@ namespace UnknownStore.Common.DataTransferObjects.Get
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
+        public IEnumerable<GetCityDto> Cities { get; set; }
 
 
         public void Mapping(Profile profile)
         {
             profile.CreateMap<Country, GetCountryDto>()
                 .ForMember(dto => dto.Title, opt => opt.MapFrom(c => c.Title))
-                .ForMember(dto => dto.Id, opt => opt.MapFrom(c => c.Id));
+                .ForMember(dto => dto.Id, opt => opt.MapFrom(c => c.Id))
+                .ForMember(dto=>dto.Cities,opt=>opt.Ignore());
         }
     }
 }
