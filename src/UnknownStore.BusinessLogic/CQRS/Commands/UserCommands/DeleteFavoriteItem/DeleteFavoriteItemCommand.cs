ff7290e93a832ceea224b062418a2dd4ffce9304
Pavel <pavell.urusov@gmail.com>
2022-06-05 15:32:07 +0300

    added delete-favorite action

---
+++ src/UnknownStore.BusinessLogic/CQRS/Commands/UserCommands/DeleteFavoriteItem/DeleteFavoriteItemCommand.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using MediatR;
+using UnknownStore.Common.CQRS;
+
+namespace UnknownStore.BusinessLogic.CQRS.Commands.UserCommands.DeleteFavoriteItem
+{
+    public record DeleteFavoriteItemCommand(Guid UserId, Guid FavoriteItemId) : IRequest<ResponseBase>;
+}
---
+++ src/UnknownStore.BusinessLogic/CQRS/Commands/UserCommands/DeleteFavoriteItem/DeleteFavoriteItemValidator.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using UnknownStore.Common.CQRS.Validation;
+using UnknownStore.DAL.Interfaces;
+
+namespace UnknownStore.BusinessLogic.CQRS.Commands.UserCommands.DeleteFavoriteItem
+{
+    public class DeleteFavoriteItemValidator : IValidationHandler<DeleteFavoriteItemCommand>
+    {
+        private readonly IStoreDbContext _context;
+
+        public DeleteFavoriteItemValidator
+        (
+            IStoreDbContext context
+        )
+        {
+            _context = context;
+        }
+
+        public async Task<ValidationResult> Validate(DeleteFavoriteItemCommand request,
+            CancellationToken cancellationToken)
+        {
+            var user = await _context.Users.FindAsync(request.UserId);
+            if (user is null)
+                return ValidationResult.Fail(ValidationMessenger.NotFoundEntity(nameof(request.UserId)));
+            return user.FavoriteModels.FirstOrDefault(c => c.Id == request.FavoriteItemId) is null
+                ? ValidationResult.Fail(ValidationMessenger.NotFoundEntity(nameof(request.FavoriteItemId)))
+                : ValidationResult.Success;
+        }
+    }
+}
