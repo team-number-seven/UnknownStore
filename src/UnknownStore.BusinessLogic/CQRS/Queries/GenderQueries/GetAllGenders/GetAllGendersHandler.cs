b4fe097b23029eab8563812b97bc8837e73605e3
Pavel <pavell.urusov@gmail.com>
2022-06-14 17:22:09 +0300

    women

---
+++ src/UnknownStore.BusinessLogic/CQRS/Queries/GenderQueries/GetAllGenders/GetAllGendersHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using UnknownStore.Common;
+using UnknownStore.Common.CQRS;
+using UnknownStore.Common.DataTransferObjects.Get;
+using UnknownStore.DAL.Interfaces;
+
+namespace UnknownStore.BusinessLogic.CQRS.Queries.GenderQueries.GetAllGenders
+{
+    public class GetAllGendersHandler : IRequestHandler<GetAllGendersQuery, ResponseBase>
+    {
+        private readonly IStoreDbContext _context;
+        private readonly ILogger<GetAllGendersHandler> _logger;
+        private readonly IMapper _mapper;
+
+        public GetAllGendersHandler
+        (
+            IStoreDbContext context,
+            ILogger<GetAllGendersHandler> logger,
+            IMapper mapper)
+        {
+            _context = context;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        public async Task<ResponseBase> Handle(GetAllGendersQuery request, CancellationToken cancellationToken)
+        {
+            var genders = await _context.Genders.OrderBy(f => f.Title).ToListAsync(cancellationToken);
+            var gendersDtos = genders.Select(gender => _mapper.Map<GetGenderDto>(gender)).ToList();
+            _logger.LogInformation(LoggerMessages.QueryExecutedSuccessfully(nameof(GetAllGendersHandler)));
+            return new GetAllGendersResponse(gendersDtos);
+        }
+    }
+}
---
+++ src/UnknownStore.WebAPI/Controllers/GenderController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UnknownStore.BusinessLogic.CQRS.Commands.FactoryCommand;
+using UnknownStore.BusinessLogic.CQRS.Queries.FactoryQueries.GetAllFactories;
+using UnknownStore.BusinessLogic.CQRS.Queries.GenderQueries.GetAllGenders;
+using UnknownStore.Common.DataTransferObjects.Create;
+
+namespace UnknownStore.WebAPI.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("store/[controller]")]
+    public class GenderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public GenderController(
+            IMediator mediator
+        )
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("get-all")]
+        public async Task<IActionResult> GetAllGenders()
+        {
+            var response = await _mediator.Send(new GetAllGendersQuery());
+            return StatusCode((int)response.StatusCode, response);
+        }
+    }
+}
